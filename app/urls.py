3d3ddb22f9aec5d82d6757f0685cb24d5aaa7dff
shahbaz khan <sk9200919@gmail.com>
2025-01-24 17:43:43 +0500

    fix issue .gitignore

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app import views
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-urlpatterns = [
-    path('s',views.simple_view,name="s")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def simple_view(request):
-    return HttpResponse("Hello, welcome to my Django app!")
