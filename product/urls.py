21ab7ac406944d6195d433b25436920263147cf0
shahbaz khan <sk9200919@gmail.com>
2025-01-29 19:00:00 +0500

    fix bug

--- product/urls.py
+++ product/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from product import views
 urlpatterns = [
     path('list/',views.Listapi.as_view(),name='list'),
-    path('laptop/',views.Laptopapi.as_view(),name='create'),
-    path('mobile/',views.Mobileapi.as_view(),name='update'),
+    path('laptop/',views.Laptopapi.as_view(),name='laptop'),
+    path('mobile/',views.Mobileapi.as_view(),name='mobile'),
     path('topwear/',views.Topwearapi.as_view(),name='topwear'),
     path('buttomwear/',views.Buttomwearapi.as_view(),name='buttomwear'),
 ]
