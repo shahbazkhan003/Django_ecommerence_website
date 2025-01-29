92f8f9ee25e08c403acaa5e8003070c4d3581015
Sarmad <sarmadkhalique001@gmail.com>
2025-01-29 16:08:17 +0500

    update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path,include
-from . import views
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_registration.api.urls import register
 
 
 urlpatterns = [    
-    path('', include('rest_registration.api.urls')),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', register, name='register'),
 ]
 
--- ecommerence_web/urls.py
+++ ecommerence_web/urls.py
@@ -16,14 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-from django.urls import path, include
-from  accounts.urls import api_urlpatterns 
 
 urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
-    path('api/v1/', include(api_urlpatterns)),
-    path('',include('accounts.urls')),
+    path('api/v1/accounts/',include('accounts.urls')),
 ]
